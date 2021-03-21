4a08367e6c601f15abc7f50e2d0ef012404261ec
christos-kouros <56131091+christos-kouros@users.noreply.github.com>
2021-03-21 21:18:50 +0200

    Update Client.java

--- src/ClientSide/Client.java
+++ src/ClientSide/Client.java
@@ -1,9 +1,8 @@
-import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Socket socket = null;
 
 
